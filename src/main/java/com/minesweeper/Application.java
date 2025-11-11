a3da521848d09ede72ad872e29cd897bac8743e8
WEN <22twen@stu.edu.cn>
2025-11-11 09:07:54 +0800

    first commit

--- src/main/java/com/minesweeper/Application.java
+++ src/main/java/com/minesweeper/Application.java
@@ -1,8 +1,5 @@
 package com.minesweeper;
 
-
-import com.minesweeper.repository.PlayerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/test/java/com/minesweeper/ApplicationTests.java
+++ src/test/java/com/minesweeper/ApplicationTests.java
@@ -2,7 +2,6 @@ package com.minesweeper;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 // TODO 引导类
